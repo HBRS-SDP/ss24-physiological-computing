03da59478e63de4451b6a745ed04cc2d8439c149
Elektra-V <vedika.chauhan@outlook.com>
2024-08-16 15:00:00 +0200

    Add thread manager

---
+++ HriPhysioLib/src/utilities/helpers.h
@@ -0,0 +1,21 @@
+#ifndef HRI_PHYSIO_HELPERS_H
+#define HRI_PHYSIO_HELPERS_H
+
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+std::string to_uppercase(std::string &input_string);
+
+std::string to_lowercase(std::string &input_string);
+
+std::vector<std::string> parse_string(const std::string &string);
+
+std::string combine_string(const std::vector<std::string> &source, size_t idx = 0);
+
+std::vector<double> to_vec_double(const std::vector<std::string> &source, size_t idx = 0);
+
+#endif //HRI_PHYSIO_HELPERS_H
