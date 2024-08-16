4af6f3bfccbaca555a07c67e691c7fe270391902
ItsShriks <nakhyeshrikar@icloud.com>
2024-08-16 15:58:20 +0200

    add parser

---
+++ HriPhysioLib/src/utilities/arg_parser.h
@@ -0,0 +1,17 @@
+#ifndef HRI_PHYSIO_ARG_PARSER_H
+#define HRI_PHYSIO_ARG_PARSER_H
+
+#include <iostream>
+#include <vector>
+
+class ArgParser {
+public:
+    ArgParser(int &argc, char **argv);
+
+    [[nodiscard]] std::string get_cmd_option(const std::string &option) const;
+
+private:
+    std::vector<std::string> tokens;
+};
+
+#endif //HRI_PHYSIO_ARG_PARSER_H
