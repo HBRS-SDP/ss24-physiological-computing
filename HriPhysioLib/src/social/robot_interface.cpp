36c92b55c44f23cb83fc028d2ded476b99ee8d17
Elektra-V <vedika.chauhan@outlook.com>
2024-08-20 11:00:15 +0200

    Cleanup

---
+++ HriPhysioLib/src/social/robot_interface.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Vedika Chauhan on 27.08.24.
+//
+#include "robot_interface.h"
+
+RobotInterface::RobotInterface() = default;
---
+++ HriPhysioLib/src/social/robot_interface.h
@@ -0,0 +1,42 @@
+#ifndef HRI_PHYSIO_ROBOT_INTERFACE_H
+#define HRI_PHYSIO_ROBOT_INTERFACE_H
+
+#include <string>
+#include <iostream>
+
+enum Peripheral {
+    HEAD, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG
+};
+
+class RobotInterface {
+protected:
+    std::string name;
+
+public:
+    RobotInterface();
+
+    virtual bool configure(int argc, char **argv) = 0;
+
+    virtual void robot_loop() = 0;
+
+    virtual bool get_robot_command(std::string &command) = 0;
+
+    virtual bool add_audio_file(std::string filename, size_t channel = -1) = 0;
+
+    virtual bool add_video_file(std::string filename) = 0;
+
+    virtual bool set_periph_state(Peripheral perph, std::vector<double> &position) = 0;
+
+    virtual bool add_gesture(std::string gesture, double speed = 1.0) = 0;
+
+    virtual bool set_emotion_state(std::string emotion) = 0;
+
+    virtual bool add_speech(std::string phrase) = 0;
+
+    virtual bool set_speech_config(std::string config) = 0;
+
+    virtual bool set_volume(double percent) = 0;
+};
+
+
+#endif //HRI_PHYSIO_ROBOT_INTERFACE_H
