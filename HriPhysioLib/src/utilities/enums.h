89b9742ca1e86d8e37de943b5c68cd41275d2abd
ItsShriks <nakhyeshrikar@icloud.com>
2024-08-24 14:16:22 +0200

    Utilities > enums

---
+++ HriPhysioLib/src/utilities/enums.h
@@ -0,0 +1,12 @@
+#ifndef HRI_PHYSIO_ENUMS_H
+#define HRI_PHYSIO_ENUMS_H
+
+enum ModeTag {
+    NOT_SET, SENDER, RECEIVER
+};
+
+enum VarTag {
+    CHAR, INT16, INT32, INT64, LONG_LONG, FLOAT, DOUBLE, STRING
+};
+
+#endif //HRI_PHYSIO_ENUMS_H
