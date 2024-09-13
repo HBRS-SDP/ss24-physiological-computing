b275a8193bfa7d310e8b7b8ce7dfd9dad2b64848
ItsShriks <nakhyeshrikar@icloud.com>
2024-09-14 01:07:30 +0200

    Add Licence

--- HriPhysioLib/lib/src/utilities/helpers.cpp
+++ HriPhysioLib/lib/src/utilities/helpers.cpp
@@ -1,12 +1,23 @@
 /* ================================================================================
- * Copyright: (C) 2020, SIRRL Social and Intelligent Robotics Research Laboratory, 
+ * Copyright: (C) 2021, SIRRL Social and Intelligent Robotics Research Laboratory, 
  *     University of Waterloo, All rights reserved.
- *     Hochschule Bonn-Rhein-Sieg, All rights reserved.
  * 
  * Authors: 
  *     Austin Kothig <austin.kothig@uwaterloo.ca>
+ * 
+ * CopyPolicy: Released under the terms of the BSD 3-Clause License. 
+ *     See the accompanying LICENSE file for details.
+ * ================================================================================
+ */
+
+/* ================================================================================
+ * Copyright: (C) 2024, Shrikar Nakhye,
+ *     Hochschule Bonn-Rhein-Sieg (H-BRS), All rights reserved.
+ * 
+ * Author: 
  *     Shrikar Nakhye <shrikar.nakhye@smail.inf.h-brs.de>
- * CopyPolicy: Released under the terms of the MIT License. 
+ * 
+ * CopyPolicy: Released under the terms of the MIT License.
  *     See the accompanying LICENSE file for details.
  * ================================================================================
  */
